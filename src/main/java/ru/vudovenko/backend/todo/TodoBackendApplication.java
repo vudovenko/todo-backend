62672b1f25d26afe20b4a8e80ede73b52ce30702
vudovenko <vladislav.udovenko0@yandex.ru>
2024-07-30 14:01:02 +0500

    git init

---
+++ src/main/java/ru/vudovenko/backend/todo/TodoBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.vudovenko.backend.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vudovenko/backend/todo/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vudovenko.backend.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
